103fe5ee8afc6c865c0e65a8c4e36dca241fcbc4
SSolanoB <s.solano586@uniandes.co>
2018-10-04 18:25:54 -0500

    Add devise controller for users sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_scope :user do
+  get 'users/sign_out', to: 'users/sessions#destroy'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   resources :calories
-  devise_scope :user do
-  get 'users/sign_out', to: 'users/sessions#destroy'
-  end
 end
