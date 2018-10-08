902697365d3fdd04469a9b20aafaea94cca64465
SSolanoB <s.solano586@uniandes.co>
2018-10-07 21:08:27 -0500

    Delete files in track_resets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  #get 'track_resets/new'
-  #get 'track_resets/edit'
   devise_for :users, controllers: { sessions: 'users/sessions' }
   devise_scope :user do
   get 'users/sign_out', to: 'users/sessions#destroy'
