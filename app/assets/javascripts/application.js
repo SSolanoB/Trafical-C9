dfeac1798bc1df517cd82259381c5efc7ab99cb1
SSolanoB <s.solano586@uniandes.co>
2018-10-07 20:16:39 -0500

    Change place of application js code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,14 +23,8 @@ $(document).on('click', '.notification > button.delete', function() {
     return false;
 });
 
-$(document).ready(function() {
-
-  // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
-
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-
-  });
+$(document).on('click', '.nav-brand > button.navbar-burger'function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
 });
+
