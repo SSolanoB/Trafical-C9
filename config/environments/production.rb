ef59d706554436ec348c82753af04119ff9ce801
SSolanoB <s.solano586@uniandes.edu.co>
2018-10-09 22:19:58 +0000

    Change host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'trafical.herokuapp.com'
+  host = 'traficalc9.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
